68e8d7c863f9528415443c98072d10c407383b15
Piotr Grabowski <fau999@gmail.com>
2022-07-11 17:33:41 +0200

    qual(notify): bump notification display time for errors

--- src/web/common/notify/notify.ts
+++ src/web/common/notify/notify.ts
@@ -4,13 +4,20 @@ declare const Toastify: any;
 
 export type NotificationKind = "info" | "warning" | "error";
 
+const DURATIONS_MS: Record<NotificationKind, number> = {
+  "info": 5000,
+  "warning": 5000,
+  "error": 10000,
+}
+
 export function notify(kind: NotificationKind, text: string) {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call
   const toast = new Toastify({
     className: kind.toString().toLowerCase(),
+    duration: DURATIONS_MS[kind],
     text,
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-call
-    onClick: () => { toast.hideToast(); },
+    onClick: () => toast.hideToast(),
   });
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-call
   toast.showToast();
