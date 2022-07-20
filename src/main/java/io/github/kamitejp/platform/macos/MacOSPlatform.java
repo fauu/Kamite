fea275ab6830e1a1f05d2230eb31dbaef19e44c0
Piotr Grabowski <fau999@gmail.com>
2022-07-20 15:56:00 +0200

    feat(macos): add macOS support stub

---
+++ src/main/java/io/github/kamitejp/platform/macos/MacOSPlatform.java
@@ -0,0 +1,124 @@
+package io.github.kamitejp.platform.macos;
+
+import java.awt.MouseInfo;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.tulskiy.keymaster.common.Provider;
+
+import io.github.kamitejp.geometry.Point;
+import io.github.kamitejp.geometry.Rectangle;
+import io.github.kamitejp.platform.GenericPlatform;
+import io.github.kamitejp.platform.GlobalKeybindingProvider;
+import io.github.kamitejp.platform.OS;
+import io.github.kamitejp.platform.Platform;
+import io.github.kamitejp.platform.PlatformCreationException;
+import io.github.kamitejp.platform.RecognitionOpError;
+import io.github.kamitejp.platform.RobotScreenshoter;
+import io.github.kamitejp.platform.RobotScreenshoterUnavailableException;
+import io.github.kamitejp.platform.windows.AreaSelectorFrame; // XXX (DEV)
+import io.github.kamitejp.util.Result;
+
+@SuppressWarnings("PMD") // DEV
+public class MacOSPlatform extends GenericPlatform implements Platform, GlobalKeybindingProvider {
+  private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+  private AreaSelectorFrame selector;
+  private final RobotScreenshoter robotScreenshoter;
+  private Provider keymasterProvider;
+
+  public MacOSPlatform() throws PlatformCreationException {
+    super("macos");
+
+    if (getOS() != OS.MACOS) {
+      throw new PlatformCreationException("Detected OS is not macOS");
+    }
+
+    try {
+      robotScreenshoter = new RobotScreenshoter();
+    } catch (RobotScreenshoterUnavailableException e) {
+      throw new PlatformCreationException("Could not initialize Robot Screenshoter", e);
+    }
+  }
+
+  @Override
+  public Path getMangaOCRWrapperPath() {
+    // DEV: Incomplete
+    return null;
+  }
+
+  @Override
+  public Result<Point, RecognitionOpError> getUserSelectedPoint() {
+    return Result.Ok(Point.from(MouseInfo.getPointerInfo().getLocation()));
+  }
+
+  @Override
+  public Result<Rectangle, RecognitionOpError> getUserSelectedArea() {
+    this.selector = new AreaSelectorFrame();
+    selector.setVisible(true);
+
+    var futureArea = selector.getRegion();
+    var area = futureArea.join();
+    selector.setVisible(false);
+
+    return Result.Ok(area);
+  }
+
+  @Override
+  public Result<BufferedImage, RecognitionOpError> takeAreaScreenshot(Rectangle area) {
+    var maybeScreenshot = robotScreenshoter.takeScreenshotOfArea(area.toAWT());
+    if (maybeScreenshot.isEmpty()) {
+      return Result.Err(RecognitionOpError.SCREENSHOT_FAILED);
+    }
+
+    // XXX: (DEV)
+    // GenericPlatform.writeImage(
+    //   maybeScreenshot.get(),
+    //   Paths.get(System.getProperty("user.home")).resolve("Pictures").resolve("kamite-test.png")
+    // );
+
+    return Result.Ok(maybeScreenshot.get());
+  }
+
+  @Override
+  public void openURL(String url) {
+    try {
+      Runtime.getRuntime().exec(new String[] {
+        "osascript", "-e", "open location \"%s\"".formatted(url)
+      });
+    } catch (IOException e) {
+      LOG.warn("Could not open web browser", e);
+    }
+  }
+
+  @Override
+  public Optional<Path> getConfigDirPath() {
+    return Optional.of(
+      Paths.get(System.getProperty("user.home"))
+        .resolve("Library/Application Support")
+        .resolve(CONFIG_DIR_PATH_RELATIVE)
+    );
+  }
+
+  @Override
+  public void destroy() {
+    destroyKeybindings();
+  }
+
+  @Override
+  public Provider getKeymasterProvider() {
+    return keymasterProvider;
+  }
+
+  @Override
+  public void setKeymasterProvider(Provider provider) {
+    this.keymasterProvider = provider;
+  }
+}
