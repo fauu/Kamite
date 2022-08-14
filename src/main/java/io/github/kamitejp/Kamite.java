fdb18c1fc8120430d5ab7ec3018a64ea7d7c04e4
Piotr Grabowski <fau999@gmail.com>
2022-08-14 15:47:42 +0200

    qual: improve debug message

--- src/main/java/io/github/kamitejp/Kamite.java
+++ src/main/java/io/github/kamitejp/Kamite.java
@@ -462,7 +462,7 @@ public class Kamite {
       case SCREENSHOT_API_COMMUNICATION_FAILED ->
         LOG.error("Failed to communicate with the screenshot API");
       case SELECTION_CANCELLED ->
-        LOG.debug("Screen area selection was cancelled by user");
+        LOG.debug("Screen area selection was cancelled by the user");
       case SELECTION_FAILED ->
         LOG.error("Failed to perform screen area selection");
       case SCREENSHOT_FAILED ->
