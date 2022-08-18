6c5c2195c9fcf7ce735611b101adb1d959d9bd3f
Piotr Grabowski <fau999@gmail.com>
2022-08-18 15:11:07 +0200

    improve(launcher): don't show console window on Windows

--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -1,3 +1,5 @@
+#![windows_subsystem = "windows"]
+
 use std::{
     env,
     process::{Command, Stdio},
@@ -7,17 +9,14 @@ use std::{
 use std::os::windows::process::CommandExt;
 
 #[cfg(windows)]
-const CREATE_NO_WINDOW: u32 = 0x08000000;
+use winapi::um::winbase;
 
 #[cfg(unix)]
 const JAVA_EXE_NAME: &str = "java";
-
 #[cfg(windows)]
 const JAVA_EXE_NAME: &str = "java.exe";
 
 fn main() {
-    let args: Vec<String> = env::args().skip(1).collect();
-
     let mut current_exe_dir =
         env::current_exe().expect("Could not determine current executable's path");
     current_exe_dir.pop();
@@ -30,48 +29,43 @@ fn main() {
     java_path.push("runtime");
     java_path.push("bin");
     java_path.push(JAVA_EXE_NAME);
-    let java_path_str = java_path
-        .into_os_string()
-        .into_string()
-        .expect("Could not convert java path to string");
 
     let mut jar_path = current_exe_dir.clone();
     jar_path.push("lib");
     jar_path.push("generic");
     jar_path.push("kamite.jar");
 
-    let mut cmd = Command::new(if cfg!(windows) {
-        "cmd".to_owned()
-    } else {
-        java_path_str.clone()
-    });
-    if cfg!(windows) {
-        cmd.arg("/C");
-        cmd.arg(java_path_str);
-    }
+    let mut cmd = Command::new(java_path);
     cmd.arg("--enable-preview");
     if cfg!(target_os = "linux") {
         cmd.args(&["--add-opens", "java.desktop/sun.awt.X11=ALL-UNNAMED"]);
     }
-    cmd.arg("-jar")
-        .arg(jar_path)
-        .args(&args)
-        .stdin(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit());
+    cmd.arg("-jar").arg(jar_path).args(env::args().skip(1));
 
-    cmd_build_platform_specific(&mut cmd, &args);
+    if cfg!(unix) {
+        cmd.stdin(Stdio::inherit())
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit());
+    } else {
+        cmd.stdin(Stdio::null())
+            .stdout(Stdio::null())
+            .stderr(Stdio::null());
+    };
+
+    cmd_build_platform_specific(&mut cmd);
 
     let mut child = cmd.spawn().expect("Failed to launch Kamite");
-    child.wait().expect("Process was not running");
-}
-
-#[cfg(windows)]
-fn cmd_build_platform_specific(cmd: &mut Command, args: &[String]) {
-    if args.iter().all(|arg| !arg.starts_with("--debug")) {
-        cmd.creation_flags(CREATE_NO_WINDOW);
+    if cfg!(unix) {
+        child.wait().expect("Process was not running");
     }
 }
 
+#[cfg(windows)]
+fn cmd_build_platform_specific(cmd: &mut Command) {
+    cmd.creation_flags(
+        winbase::DETACHED_PROCESS | winbase::CREATE_NEW_PROCESS_GROUP | winbase::CREATE_NO_WINDOW,
+    );
+}
+
 #[cfg(unix)]
-fn cmd_build_platform_specific(_cmd: &mut Command, _args: &[String]) {}
+fn cmd_build_platform_specific(_cmd: &mut Command) {}
