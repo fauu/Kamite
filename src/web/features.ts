c0daa4b13983d30f5159cab42d366d986f8b342b
Piotr Grabowski <fau999@gmail.com>
2022-09-09 11:59:06 +0200

    improve(ux): link directly to the relevant README section in Show furigana unavailable msg

--- src/web/features.ts
+++ src/web/features.ts
@@ -1,4 +1,5 @@
-const README_URL = "https://github.com/fauu/Kamite/blob/master/README.md";
-export const README_LINK = `<a href="${README_URL}" target="_blank">README</a>`;
+const AUTO_FURIGANA_README_URL = "https://github.com/fauu/Kamite#auto-generated-furigana";
+export const AUTO_FURIGANA_README_LINK =
+  `<a href="${AUTO_FURIGANA_README_URL}" target="_blank">README</a>`;
 export const SHOW_FURIGANA_DISABLED_MSG =
-  `This feature requires an extra download. For details, see the ${README_LINK}.`;
+  `This feature requires an extra download. For details, see the ${AUTO_FURIGANA_README_LINK}.`;
