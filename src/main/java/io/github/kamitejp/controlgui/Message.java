0b5b58a859276de171a04dd27dd148fd9c367194
Piotr Grabowski <fau999@gmail.com>
2023-02-11 15:35:49 +0100

    improve(controlgui): improve message layout

--- src/main/java/io/github/kamitejp/controlgui/Message.java
+++ src/main/java/io/github/kamitejp/controlgui/Message.java
@@ -1,10 +1,52 @@
 package io.github.kamitejp.controlgui;
 
-import javax.swing.JLabel;
+import java.awt.Color;
 
-public class Message extends JLabel {
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Message extends JPanel {
+  private static final int HORIZONTAL_GAP = 10;
+  private static final int VERTICAL_GAP = 20;
+
+  private static Integer typeLabelWidth;
+
   public Message(String timeString, Type type, String content) {
-    super("<html>%s <i>%s</i> %s</html>".formatted(timeString, type, content));
+    var layout = new GroupLayout(this);
+    setLayout(layout);
+    setBackground(new Color(0x484542)); // XXX: Constant in ControlGUI
+
+    if (typeLabelWidth == null) {
+      // QUAL: Better to use getStringBounds() or something of the sort?
+      typeLabelWidth = (int) new JLabel(Type.WARNING.toString()).getPreferredSize().getWidth();
+    }
+
+    var timeLabel = new JLabel(timeString);
+    var typeLabel = new JLabel(type.toString());
+    var contentLabel = new JLabel("<html>%s</html>".formatted(content));
+
+    var def = GroupLayout.DEFAULT_SIZE;
+    var pref = GroupLayout.PREFERRED_SIZE;
+    var max = Short.MAX_VALUE;
+    layout.setHorizontalGroup(
+      layout.createSequentialGroup()
+        .addComponent(timeLabel, pref, pref, pref)
+        .addGap(HORIZONTAL_GAP)
+        .addComponent(typeLabel, typeLabelWidth, typeLabelWidth, typeLabelWidth)
+        .addGap(HORIZONTAL_GAP)
+        .addComponent(contentLabel, def, def, max)
+    );
+    layout.setVerticalGroup(
+      layout.createParallelGroup(Alignment.LEADING, false)
+        .addComponent(timeLabel, pref, pref, pref)
+        .addGap(VERTICAL_GAP)
+        .addComponent(typeLabel, pref, pref, pref)
+        .addGap(VERTICAL_GAP)
+        .addComponent(contentLabel, pref, pref, max)
+        .addGap(VERTICAL_GAP)
+    );
   }
 
   public static enum Type {
