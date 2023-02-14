40903c56615cfdbf65e9b7ff519ef6e3399dab6b
Piotr Grabowski <fau999@gmail.com>
2023-02-14 22:09:08 +0100

    chore(debug): add solid-devtools

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,17 @@
 import path from "path";
 
 import { defineConfig } from "vite";
+import solidDevtools from 'solid-devtools/vite'
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
   root: "src/web",
-  plugins: [solidPlugin()],
+  plugins: [
+    solidDevtools({
+      autoname: true
+    }),
+    solidPlugin()
+  ],
   build: {
     outDir: "../../target/web",
     target: 'esnext',
