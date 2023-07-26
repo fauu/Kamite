30f5e80ab1faaacdbb0a2a27415d9e4e9e3e292c
Piotr Grabowski <fau999@gmail.com>
2023-07-26 17:41:53 +0200

    fix(ocr): fix HiveOCR results displaying newlines as the literal string '\n'

--- src/main/java/io/github/kamitejp/recognition/HiveOCRHFAdapter.java
+++ src/main/java/io/github/kamitejp/recognition/HiveOCRHFAdapter.java
@@ -5,6 +5,8 @@ import java.lang.invoke.MethodHandles;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import io.github.kamitejp.util.Result;
+
 public class HiveOCRHFAdapter extends BaseHFOCRAdapter {
   private static final Logger LOG = LogManager.getLogger(MethodHandles.lookup().lookupClass());
 
@@ -15,5 +17,12 @@ public class HiveOCRHFAdapter extends BaseHFOCRAdapter {
       /* responseTrimStartMarker */ "\"data\":[\"",
       /* responseTrimEndMarker */   "\",[{\"text"
     );
+  }
+
+  @Override
+  protected Result<String, RemoteOCRRequestError> trimmedResponseToOCRText(String res) {
+    // QUAL: Ad-hoc fix
+    res = res.replaceAll("\\\\n", "\n");
+    return Result.Ok(res);
   }
 }
